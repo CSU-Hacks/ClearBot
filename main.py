e079984f2c81bb6843a2673feabe78ceb0f9a8c9
Nick Cleary <nicholascleary01@gmail.com>
2020-08-01 13:06:55 -0400

    Begun work on figuring out table structure and added logging

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 #main driver file for Clearbot
 
-import discord
+import discord, logging
+from discord.ext import commands
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
+
+bot = commands.Bot(command_prefix='!')
 
 def get_token():
     token = ''
@@ -8,10 +17,26 @@ def get_token():
         token += file.read()
     return token
 
-client = discord.Client()
-
-@client.event
+@bot.event
 async def on_ready():
     print('Client is online!')
 
-client.run(get_token())
+@bot.event
+async def on_guild_join(server):
+    print(str(server.id))
+
+@bot.event
+async def on_guild_remove(server):
+    print(str(server.id))
+
+@bot.command()
+async def test(ctx, *args):
+    print('it happens')
+    await ctx.send(args)
+"""
+@bot.command()
+async def other_test(ctx, *args):
+    print('ruh roh')
+    await ctx.send(args)
+"""
+bot.run(get_token())
